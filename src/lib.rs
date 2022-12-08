e5698e7823bb059273a6e06e6c18f58083d98413
test <1041008319@qq.com>
2022-12-08 16:42:15 +0800

    双向链表部分完成，LRUCache部分TODO

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,140 @@
 pub mod double_list {
     use std::cell::RefCell;
+    use std::collections::HashMap;
+    use std::fmt::write;
     use std::rc::Rc;
 
     #[derive(Debug)]
     pub struct ListNode {
+        key: i32,
         value: i32,
         next: Option<Rc<RefCell<ListNode>>>,
         prev: Option<Rc<RefCell<ListNode>>>
     }
 
     pub struct List {
-        len: usize,
-        head: Option<Rc<RefCell<ListNode>>>,
-        tail: Option<Rc<RefCell<ListNode>>>
+        len: i32,
+        pub head: Option<Rc<RefCell<ListNode>>>,
+        pub tail: Option<Rc<RefCell<ListNode>>>
+    }
+
+    pub struct LRUCache {
+        map: HashMap<i32, Option<Rc<RefCell<ListNode>>>>,
+        list: List,
+        size: i32,
+        cap: i32,
     }
 
     impl ListNode {
-        pub fn new(value: i32) -> Rc<RefCell<ListNode>> {
-            let tmp = Rc::new(RefCell::new(ListNode{
-                value,
+        pub fn new(k: i32, v: i32) -> Rc<RefCell<ListNode>> {
+            Rc::new(RefCell::new(ListNode{
+                key: k,
+                value: v,
                 next: None,
                 prev: None
-            }));
-            return Rc::clone(&tmp);
+            }))
+        }
+        pub fn display(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{:?} : {:?} =>", self.key, self.value)?;
+            if let Some(node) = &self.next {
+                return node.borrow().display(f);
+            }
+            write!(f,"")
         }
     }
 
     impl List {
         pub fn new() -> List {
-            let first = ListNode::new(0);
-            let last = ListNode::new(0);
-            first.borrow().next = Some(Rc::clone(&last));
-            last.borrow().next = Some(Rc::clone(&first));
+            let head = ListNode::new(0, 0);
+            let tail = ListNode::new(0, 0);
+            head.borrow_mut().next = Some(Rc::clone(&tail));
+            tail.borrow_mut().next = Some(Rc::clone(&head));
             return List {
                 len: 0,
-                head: Some(first),
-                tail: Some(last)
+                head: Some(head),
+                tail: Some(tail)
             }
         }
 
-        pub fn insert(&mut self, index: i32, value: i32) {
+        pub fn add_to_head(&mut self, node: Option<Rc<RefCell<ListNode>>>) {
+            if let Some(h) = self.head.take() {
+                if let Some(n) = &node {
+                    let hnxt = h.borrow_mut().next.take();
+                    n.borrow_mut().prev = self.head.clone();
+                    n.borrow_mut().next = hnxt.clone();
+                    if let Some(hn) = &hnxt {
+                        hn.borrow_mut().prev = node.clone();
+                    }
+                    h.borrow_mut().next = node.clone();
+                }
+                self.head = Some(h);
+                self.len += 1;
+            }
+        }
 
+        pub fn remove_node(&mut self, node: Option<Rc<RefCell<ListNode>>>) {
+            if let Some(n) = &node {
+                let pre = n.borrow_mut().prev.take();
+                let nxt = n.borrow_mut().next.take();
+                if let Some(p) = &pre {
+                    p.borrow_mut().next = nxt.clone();
+                }
+                if let Some(nx) = &nxt {
+                    nx.borrow_mut().prev = pre.clone();
+                }
+                // n.borrow_mut().prev = pre.clone();
+                // n.borrow_mut().next = nxt.clone();
+                self.len -= 1;
+            }
+        }
+
+        pub fn move_to_head(&mut self, node: Option<Rc<RefCell<ListNode>>>) {
+            let ncl = node.clone();
+            self.remove_node(node);
+            self.add_to_head(ncl);
+        }
+
+        pub fn remove_tail(&mut self) -> i32{
+            let mut ans = 0;
+            if let Some(t) = self.tail.take() {
+                let node = t.borrow_mut().prev.take();
+                if let Some(n) = &node {
+                    ans = n.borrow_mut().key;
+                }
+                self.tail = Some(t);
+                self.remove_node(node);
+            }
+            return ans
+        }
+
+        pub fn show_all(&mut self) {
+            if let Some(n) = self.head.take() {
+                let mut node = n.clone();
+                for i in 1..self.len+2 {
+                    if i != 1 && i != self.len + 2{
+                        print!("key: {:?}, ", node.borrow().key);
+                        println!("value: {:?}", node.borrow().value);
+                    }
+                    let tmp = node.borrow().next.as_ref().unwrap().clone();
+                    node = tmp;
+                }
+                self.head = Some(n)
+            }
+        }
+
+    }
+
+    impl LRUCache {
+
+        //TODO
+        pub fn get(key: i32) -> i32{
+            return 0;
+        }
+
+        //TODO
+        pub fn put(key: i32, val: i32) -> i32 {
+            return 0;
         }
     }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 mod lib;
 
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::rc::Rc;
 use lib::double_list::ListNode;
 use lib::double_list::List;
 
-fn main() {
-    let list : Rc<RefCell<ListNode>> = ListNode::new(50);
+fn new_rc(k: i32, v: i32)-> Option<Rc<RefCell<ListNode>>> {
+    let node = ListNode::new(k, v);
+    return Some(node);
+}
 
+fn test() {
+    let mut mp = HashMap::new();
+    let mut list = List::new();
+    let node1 = new_rc(1, 1);
+    let node2 = new_rc(2, 2);
+    let node3 = new_rc(3, 3);
+    mp.insert(1, node1.clone());
+    //println!("{}", list.head.is_none());
+    list.add_to_head(node1);
+    //println!("{}", list.head.is_none());
+    list.add_to_head(node2);
+    list.add_to_head(node3);
+    list.show_all();
+    let x = 1;
+    if let Some(t) = mp.get(&x) {
+        list.move_to_head(t.clone());
+    }
+    list.show_all();
+}
+
+fn main() {
+    test();
     println!("Hello, world!");
 }
